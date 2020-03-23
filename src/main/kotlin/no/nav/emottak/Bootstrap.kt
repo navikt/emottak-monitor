4b23048421c0dd1f71f76803a882df9840963dd7
Joakim Kartveit <joakimkartveit@gmail.com>
2020-03-23 13:15:07 +0100

    Henter ut bruker navn og passord for å koble opp mot emottak databasen

--- src/main/kotlin/no/nav/emottak/Bootstrap.kt
+++ src/main/kotlin/no/nav/emottak/Bootstrap.kt
@@ -4,6 +4,7 @@ import io.ktor.util.KtorExperimentalAPI
 import no.nav.emottak.application.ApplicationServer
 import no.nav.emottak.application.ApplicationState
 import no.nav.emottak.application.createApplicationEngine
+import no.nav.emottak.util.getFileAsString
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -12,6 +13,12 @@ val log: Logger = LoggerFactory.getLogger("no.nav.emottak.emottakAdmin")
 @KtorExperimentalAPI
 fun main() {
     val environment = Environment()
+
+    val databaseVaultSecrets = VaultCredentials(
+        databasePassword = getFileAsString("/secrets/emottak-admin/credentials/password"),
+        databaseUsername = getFileAsString("/secrets/emottak-admin/credentials/username")
+    )
+    log.info("found db username and password")
 
     val applicationState = ApplicationState()
 
