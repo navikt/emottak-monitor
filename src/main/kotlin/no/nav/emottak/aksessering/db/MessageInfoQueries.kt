7cc110b2b21d3f94e7bdd7a583e0715c71580f88
Nabil Fario <nabil.fario@nav.no>
2021-06-09 15:16:30 +0200

    tester timestamp format

--- src/main/kotlin/no/nav/emottak/aksessering/db/MessageInfoQueries.kt
+++ src/main/kotlin/no/nav/emottak/aksessering/db/MessageInfoQueries.kt
@@ -5,6 +5,7 @@ import no.nav.emottak.db.toList
 import no.nav.emottak.model.MeldingInfo
 import java.sql.ResultSet
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 fun DatabaseInterface.hentMeldinger(
     databasePrefix: String,
@@ -16,8 +17,8 @@ fun DatabaseInterface.hentMeldinger(
             """
                     SELECT ROLE, SERVICE, ACTION, MOTTAK_ID, DATOMOTTAT 
                     FROM $databasePrefix.MELDING 
-                    WHERE DATOMOTTAT >= to_timestamp($fom, 'YYYY-MM-DD''T''hh:mm:ss')
-                    AND DATOMOTTAT <= to_timestamp($tom, 'YYYY-MM-DD''T''hh:mm:ss')
+                    WHERE DATOMOTTAT >= to_timestamp(${fom.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))}, 'YYYY-MM-DD HH24:MI:SS')
+                    AND DATOMOTTAT <= to_timestamp(${fom.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))}, 'YYYY-MM-DD HH24:MI:SS')
                """
         ).use {
             it.executeQuery().toList { toMeldingInfo() }
