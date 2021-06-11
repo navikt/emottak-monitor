e87922be789dd6afd73eea6bb12dc1d1ab50edd9
Nabil Fario <nabil.fario@nav.no>
2021-06-11 16:18:29 +0200

    tester timestamp format

--- src/main/kotlin/no/nav/emottak/aksessering/db/MessageInfoQueries.kt
+++ src/main/kotlin/no/nav/emottak/aksessering/db/MessageInfoQueries.kt
@@ -18,8 +18,8 @@ fun DatabaseInterface.hentMeldinger(
             """
                     SELECT ROLE, SERVICE, ACTION, MOTTAK_ID, DATOMOTTAT 
                     FROM $databasePrefix.MELDING 
-                    WHERE DATOMOTTAT >= to_timestamp('2021-01-01 09:10:20.419000', 'YYYY-MM-DD HH24:MI:SS.FF')
-                      AND DATOMOTTAT <= to_timestamp('2021-01-01 09:16:20.419000', 'YYYY-MM-DD HH24:MI:SS.FF')
+                    WHERE DATOMOTTAT >= to_timestamp($fom, 'YYYY-MM-DD HH24:MI:SS.FF')
+                      AND DATOMOTTAT <= to_timestamp($tom, 'YYYY-MM-DD HH24:MI:SS.FF')
                 """
         ).use {
             it.executeQuery().toList { toMeldingInfo() }
