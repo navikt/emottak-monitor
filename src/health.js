d08abd6124215f259100fbf01ea8340178b0c11f
Nabil Fario <nabil.fario@nav.no>
2021-06-25 10:59:49 +0200

    Implementert sortering

---
+++ src/health.js
@@ -0,0 +1,16 @@
+const logger = require('morgan')
+const prometheus = require('prom-client')
+const request = require('request-promise')
+
+exports.isAlive = () => {
+    return (req, res) => {
+        res.status(200).end()
+    }
+}
+
+exports.metrics = () => {
+    return (req, res) => {
+        res.set('Content-Type', prometheus.register.contentType)
+        res.end(prometheus.register.metrics())
+    }
+}
