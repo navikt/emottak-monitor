3c6653172d0fdf2125c45101eb2038be9bcbf767
Nabil Fario <nabil.fario@nav.no>
2021-06-25 14:15:22 +0200

    Implementert sortering

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<BrowserRouter><App /></BrowserRouter>);
   const linkElement = screen.getByText(/eMottak meldinger/);
   expect(linkElement).toBeInTheDocument();
 });
