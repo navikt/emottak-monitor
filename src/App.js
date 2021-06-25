6da7426237aae643118824bddced23584503fd95
Nabil Fario <nabil.fario@nav.no>
2021-06-25 10:34:35 +0200

    Implementert sortering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,110 @@
-import React, {Component} from "react"
-import MeldingList from "./MeldingList";
+import React from "react"
+import "nav-frontend-tabell-style";
+import Data from "./data/meldinger.json"
 
+const useSortableData = (items, config = null) => {
+    const [sortConfig, setSortConfig] = React.useState(config);
 
-class App extends Component {
-  render (){
+    const sortedItems = React.useMemo(() => {
+        let sortableItems = [...items];
+        if (sortConfig !== null) {
+            sortableItems.sort((a, b) => {
+                if (a[sortConfig.key] < b[sortConfig.key]) {
+                    return sortConfig.direction === 'ascending' ? -1 : 1;
+                }
+                if (a[sortConfig.key] > b[sortConfig.key]) {
+                    return sortConfig.direction === 'ascending' ? 1 : -1;
+                }
+                return 0;
+            });
+        }
+        return sortableItems;
+    }, [items, sortConfig]);
+
+    const requestSort = (key) => {
+        let direction = 'ascending';
+        if (
+            sortConfig &&
+            sortConfig.key === key &&
+            sortConfig.direction === 'ascending'
+        ) {
+            direction = 'descending';
+        }
+        setSortConfig({ key, direction });
+    };
+
+    return { items: sortedItems, requestSort, sortConfig };
+};
+
+const ProductTable = (props) => {
+    const { items, requestSort, sortConfig } = useSortableData(props.products);
+    const getClassNamesFor = (name) => {
+        if (!sortConfig) {
+            return;
+        }
+        return sortConfig.key === name ? sortConfig.direction : undefined;
+    };
     return (
-        <div>
-          <h1>eMottak meldinger</h1>
-          <MeldingList/>
+        <table className="tabell tabell--stripet">
+            <caption>Meldinger</caption>
+            <thead>
+            <tr>
+                <th>
+                    <button
+                        type="button"
+                        onClick={() => requestSort('role')}
+                        className={getClassNamesFor('role')}>Role
+                    </button>
+                </th>
+                <th>
+                    <button
+                        type="button"
+                        onClick={() => requestSort('service')}
+                        className={getClassNamesFor('service')}>Service
+                    </button>
+                </th>
+                <th>
+                    <button
+                        type="button"
+                        onClick={() => requestSort('action')}
+                        className={getClassNamesFor('action')}>Action
+                    </button>
+                </th>
+                <th>
+                    <button
+                        type="button"
+                        onClick={() => requestSort('mottakid')}
+                        className={getClassNamesFor('mottakid')}>MottakId
+                    </button>
+                </th>
+                <th>
+                    <button
+                        type="button"
+                        onClick={() => requestSort('datomottat')}
+                        className={getClassNamesFor('datomottat')}>Datomottat
+                    </button>
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            {items.map((meldingDetail)=>{
+                return  <tr>
+                    <td className="tabell__td--sortert">{meldingDetail.role}</td>
+                    <td>{meldingDetail.service}</td>
+                    <td>{meldingDetail.action}</td>
+                    <td>{meldingDetail.mottakid}</td>
+                    <td>{meldingDetail.datomottat}</td>
+                </tr>
+            })}
+            </tbody>
+        </table>
+    );
+};
+
+export default function App() {
+    return (
+        <div className="App">
+            <ProductTable products={Data}/>
         </div>
-    )
-  }
+    );
 }
-export default App
