f1414ca4f1593c25064adcc57a8c531697d44c9b
Nabil Fario <nabil.fario@nav.no>
2021-07-07 16:11:18 +0200

    nye spørring

--- src/test/kotlin/no/nav/emottak/TestData.kt
+++ src/test/kotlin/no/nav/emottak/TestData.kt
@@ -1,7 +1,12 @@
 package no.nav.emottak
 
 import no.nav.emottak.model.MeldingInfo
+import no.nav.emottak.model.MessageLoggInfo
 
 fun getMessages(): List<MeldingInfo> {
+    return emptyList()
+}
+
+fun getMessageLogg(): List<MessageLoggInfo> {
     return emptyList()
 }
