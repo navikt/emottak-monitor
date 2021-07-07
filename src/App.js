c7223640399ae2d50953ff3aa9bdf9ba847d5c60
Nabil Fario <nabil.fario@nav.no>
2021-07-07 11:00:22 +0200

    nye filtre

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect}  from "react"
+import React, { useState, useEffect }  from "react"
 import { Route, Switch } from 'react-router-dom'
 import "nav-frontend-tabell-style";
 import MessagesTable from "./MessagesTable";
@@ -8,17 +8,51 @@ import TimePicker from 'react-time-picker';
 import './App.css';
 import { Select } from 'nav-frontend-skjema'
 
+
+
 export default function App() {
     const [messages, setMessages] = useState([])
     const [fom, setFom] = useState(new Date().toLocaleDateString() + '');
     const [tom, setTom] = useState(new Date().toLocaleDateString() + '');
     let [fromTime, setFromTime] = useState(new Date().toLocaleTimeString() + '');
     let  [toTime, setToTime] = useState(new Date().toLocaleTimeString() + '');
+    let  [role, setRole] = useState('');
+    let  [service, setService] = useState('');
+    let  [action, setAction] = useState('');
+    let  [visibleMessages, setVisibleMessages] = useState(messages);
+
+    function filterRole(selectedRole) {
+        setRole(selectedRole)
+        setVisibleMessages([...messages.filter(function (MessageDetails) {
+            return ((selectedRole === '' || MessageDetails.role === selectedRole) &&
+                (service === '' || MessageDetails.service === service) &&
+                (action === '' || MessageDetails.action === action))
+        })]);
+    }
+    function filterService(selectedService) {
+        setService(selectedService)
+        setVisibleMessages([...messages.filter(function (MessageDetails) {
+            return ((role === '' || MessageDetails.role === role) &&
+                (selectedService === '' || MessageDetails.service === selectedService) &&
+                (action === '' || MessageDetails.action === action))
+        })]);
+    }
+    function filterAction(selectedAction) {
+        setAction(selectedAction)
+        setVisibleMessages([...messages.filter(function (MessageDetails) {
+            return ((role === '' || MessageDetails.role === role) &&
+                (service === '' || MessageDetails.service === service) &&
+                (selectedAction === '' || MessageDetails.action === selectedAction))
+        })]);
+    }
 
     useEffect(()=> {
         if (fom !== '' && tom !== '' && fromTime !== '' && toTime !== '') {
             axios.get(`https://emottak-monitor.dev.intern.nav.no/v1/hentmeldinger?fromDate=${fom}%20${fromTime}&toDate=${tom}%20${toTime}`)
-                .then(response => { setMessages(response.data)});
+                .then(response => {
+                    setMessages(response.data);
+                    setVisibleMessages(response.data)
+                });
         }
     },[fom, tom, fromTime, toTime])
 
@@ -26,85 +60,86 @@ export default function App() {
     let uniqueServices = [...new Set(messages.map(({service})=> service))]
     let uniqueActions = [...new Set(messages.map(({action})=> action))]
 
-
-
-console.log("Messages = " + messages)
+    console.log("Messages = " + messages)
     return (
         <div className="App">
             <Switch>
-                <Route exact path="/">
-                    <h1>eMottak meldinger</h1>
-                     <div>
-                         <table  id={"timetable"}>
-                             <tr>
-                                 <th>Fra og med dato: </th>
-                                 <th>
-                                     <Datepicker
-                                     locale={'nb'}
-                                     inputId="datepicker-input"
-                                     value={fom}
-                                     onChange={setFom}
-                                     inputProps={{
-                                         name: 'dateInput',
-                                         'aria-invalid': fom !== '' && isISODateString(fom) === false,
-                                     }}
-                                     calendarSettings={{ showWeekNumbers: false }}
-                                     showYearSelector={true}/>
-                                 </th>
-                                 <th>
-                                     <TimePicker
-                                     onChange={setFromTime}
-                                     value={fromTime}/>
-                                 </th>
-                             </tr>
-                             <tr>
-                                 <th>Til og med:</th>
-                                 <th>
-                                     <Datepicker
-                                         locale={'nb'}
-                                         inputId="datepicker-input"
-                                         value={tom}
-                                         onChange={setTom}
-                                         inputProps={{
-                                             name: 'dateInput',
-                                             'aria-invalid': tom !== '' && isISODateString(tom) === false,
-                                         }}
-                                         calendarSettings={{ showWeekNumbers: false }}
-                                         showYearSelector={true}/>
-                                 </th>
-                                 <th>
-                                     <TimePicker
-                                         onChange={setToTime}
-                                         value={toTime}/>
-                                 </th>
-                             </tr>
-                         </table>
-                     </div>
-                    <div>
-                        <Select label="filtrer på rolle?">
-                            <option value="">Velg rolle</option>
-                            {uniqueRoles.map((role)=>{
-                                return <option value={role}>{role}</option>
-                            })}
-                        </Select>
-                    </div>
-                    <div>
-                        <Select label="filtrer på service?">
-                            <option value="">Velg service</option>
-                            {uniqueServices.map((service)=>{
-                                return <option value={service}>{service}</option>
-                            })}
-                        </Select>
-                    </div>
-                    <div>
-                        <Select label="filtrer på action?">
-                            <option value="">Velg action</option>
-                            {uniqueActions.map((action)=>{
-                                return <option value={action}>{action}</option>
-                            })}
-                        </Select>
-                    </div>
-                     <MessagesTable messages={messages}/>
+            <Route exact path="/">
+                <h1>eMottak meldinger</h1>
+                <div>
+                    <table  id={"timetable"}>
+                        <tr>
+                            <th>Fra og med dato: </th>
+                            <th>
+                                 <Datepicker
+                                 locale={'nb'}
+                                 inputId="datepicker-input"
+                                 value={fom}
+                                 onChange={setFom}
+                                 inputProps={{
+                                     name: 'dateInput',
+                                     'aria-invalid': fom !== '' && isISODateString(fom) === false,
+                                 }}
+                                 calendarSettings={{ showWeekNumbers: false }}
+                                 showYearSelector={true}/>
+                            </th>
+                            <th>
+                                <TimePicker
+                                onChange={setFromTime}
+                                value={fromTime}/>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>Til og med:</th>
+                            <th>
+                                <Datepicker
+                                    locale={'nb'}
+                                    inputId="datepicker-input"
+                                    value={tom}
+                                    onChange={setTom}
+                                    inputProps={{
+                                        name: 'dateInput',
+                                        'aria-invalid': tom !== '' && isISODateString(tom) === false,
+                                    }}
+                                    calendarSettings={{ showWeekNumbers: false }}
+                                    showYearSelector={true}/>
+                            </th>
+                            <th>
+                                <TimePicker
+                                    onChange={setToTime}
+                                    value={toTime}/>
+                            </th>
+                        </tr>
+                    </table>
+                </div>
+                <div>
+                    <table>
+                        <tr>
+                            <th>
+                                <Select id={"select"} onChange={event => filterRole(event.target.value)}>
+                                    <option value="">Velg rolle</option>
+                                        {uniqueRoles.map((role)=>{
+                                        return <option value={role}>{role}</option>})}
+                                </Select>
+                            </th>
+                            <th>
+                                <Select id={"select"} onChange={event => filterService(event.target.value)}>
+                                    <option value="">Velg service</option>
+                                        {uniqueServices.map((service)=>{
+                                        return <option value={service}>{service}</option>})}
+                                </Select>
+                            </th>
+                            <th>
+                                <Select id={"select"} onChange={event => filterAction(event.target.value)}>
+                                    <option value="">Velg action</option>
+                                        {uniqueActions.map((action)=>{
+                                        return <option value={action}>{action}</option>})}
+                                </Select>
+                            </th>
+                        </tr>
+                    </table>
+                </div>
+                     <MessagesTable messages={visibleMessages}/>
                 </Route>
                 <Route exact path="/isalive" status={200}>
                     <h1>Alive</h1>
