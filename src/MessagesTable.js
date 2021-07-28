38dbe81a56b70cd69ba8aa9cfe1622b1b9facaa3
Nabil Fario <nabil.fario@nav.no>
2021-07-28 10:59:01 +0200

    fiks

--- src/MessagesTable.js
+++ src/MessagesTable.js
@@ -83,11 +83,7 @@ const MessagesTable = (props) => {
             {items.map((MessageDetails) => {
                 return <tr>
                     <td className="tabell__td--sortert">{MessageDetails.datomottat}</td>
-                    <td>
-                        $(messageDetails.loggcount) == 0 ? {MessageDetails.mottakid} :
-                        <Lenke href={`/logg/${MessageDetails.mottakid}`}>{MessageDetails.mottakid} </Lenke>
-                    </td>
-
+                    <td>${ MessageDetails.loggcount ? 'MessageDetails.mottakid' : <Lenke href={`/logg/${MessageDetails.mottakid}`}>{MessageDetails.mottakid} </Lenke> }</td>
                     <td>{MessageDetails.role}</td>
                     <td>{MessageDetails.service}</td>
                     <td>{MessageDetails.action}</td>
