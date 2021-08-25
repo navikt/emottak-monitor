29ff5c426fefb752fd77924523c9db97576cc6c2
Nabil Fario <nabil.fario@nav.no>
2021-08-25 14:25:18 +0200

    isql fiks

--- src/main/kotlin/no/nav/emottak/aksessering/db/EventsInfoQueries.kt
+++ src/main/kotlin/no/nav/emottak/aksessering/db/EventsInfoQueries.kt
@@ -3,7 +3,6 @@ package no.nav.emottak.aksessering.db
 import no.nav.emottak.db.DatabaseInterface
 import no.nav.emottak.db.toList
 import no.nav.emottak.model.HendelseInfo
-import no.nav.emottak.model.MeldingInfo
 import java.sql.ResultSet
 import java.time.LocalDateTime
 
@@ -17,7 +16,7 @@ fun DatabaseInterface.hentHendelser(
             """
                 SELECT LOGG.HENDELSEDATO, HENDELSE.HENDELSEDESKR, LOGG.TILLEGSINFO, LOGG.MOTTAK_ID, MELDING.ROLE,
                 MELDING.SERVICE, MELDING.ACTION, MELDING.REFERANSEPARAM, MELDING.EBCOMNAVN AS AVSENDER
-                FROM LOGG, MELDING, HENDELSE
+                FROM $databasePrefix.LOGG, $databasePrefix.MELDING, $databasePrefix.HENDELSE
                 WHERE LOGG.HENDELSE_ID = HENDELSE.HENDELSE_ID AND MELDING.MOTTAK_ID = LOGG.MOTTAK_ID
                 AND LOGG.HENDELSEDATO BETWEEN ? AND ?
                 ORDER BY LOGG.HENDELSEDATO DESC;
