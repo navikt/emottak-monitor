f3ce1ea8a60281c902b0062d5af0658c28490322
Camilla <camillamdalan@gmail.com>
2022-02-25 13:55:56 +0100

    :sparkles: Add new filter component and hook + debounce to events table too

--- src/EventsTable.tsx
+++ src/EventsTable.tsx
@@ -1,109 +1,63 @@
-import { Datepicker, isISODateString } from "nav-datovelger";
 import Ekspanderbartpanel from "nav-frontend-ekspanderbartpanel";
 import Lenke from "nav-frontend-lenker";
-import { Select } from "nav-frontend-skjema";
 import NavFrontendSpinner from "nav-frontend-spinner";
 //import Pagination from "paginering";
-import React, { useCallback, useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import TimePicker, { TimePickerValue } from "react-time-picker";
+import React, { useEffect, useMemo, useState } from "react";
+import Filter from "./components/Filter";
+import useDebounce from "./hooks/useDebounce";
 import useFetch from "./hooks/useFetch";
+import useFilter from "./hooks/useFilter";
 import useTableSorting from "./hooks/useTableSorting";
-import { initialDate, initialFilter, initialTime } from "./util";
+import Pagination from "./Pagination";
+import { initialDate, initialTime } from "./util";
 
 type EventInfo = {
-  action: string | null;
+  action: string;
   avsender: string | null;
   hendelsedato: string;
   hendelsedeskr: string;
   mottakid: string;
   referanse: string | null;
-  role: string | null;
-  service: string | null;
+  role: string;
+  service: string;
   tillegsinfo: string | null;
 };
-type FilterKey = keyof Pick<EventInfo, "role" | "service" | "action">;
 
 const EventsTable = () => {
-  const search = useLocation().search;
-  const fomParam = new URLSearchParams(search).get("fromDate");
-  const tomParam = new URLSearchParams(search).get("toDate");
-  const fromTimeParam = new URLSearchParams(search).get("fromTime");
-  const toTimeParam = new URLSearchParams(search).get("toTime");
-  const roleParam = new URLSearchParams(search).get("role");
-  const serviceParam = new URLSearchParams(search).get("service");
-  const actionParam = new URLSearchParams(search).get("action");
+  const [fromDate, setFromDate] = useState(initialDate(""));
+  const [toDate, setToDate] = useState(initialDate(""));
+  const [fromTime, setFromTime] = useState(initialTime(""));
+  const [toTime, setToTime] = useState(initialTime(""));
 
-  const [fom, setFom] = useState(initialDate(fomParam));
-  const [tom, setTom] = useState(initialDate(tomParam));
-  let [fromTime, setFromTime] = useState(initialTime(fromTimeParam));
-  let [toTime, setToTime] = useState(initialTime(toTimeParam));
-  let [filteredEvents, setFilteredEvents] = useState<EventInfo[]>([]);
+  // using debounce to not use value until there has been no new changes
+  const debouncedFromDate = useDebounce(fromDate, 200);
+  const debouncedToDate = useDebounce(toDate, 200);
+  const debouncedFromTime = useDebounce(fromTime, 200);
+  const debouncedToTime = useDebounce(toTime, 200);
 
   const { fetchState, callRequest } = useFetch<EventInfo[]>(
-    `/v1/henthendelser?fromDate=${fom}%20${fromTime}&toDate=${tom}%20${toTime}`
+    `/v1/henthendelser?fromDate=${debouncedFromDate}%20${debouncedFromTime}&toDate=${debouncedToDate}%20${debouncedToTime}`
   );
 
   const { loading, error, data: events } = fetchState;
+  let pageSize = 10;
 
-  let [filters, setFilters] = useState<Record<FilterKey, string>>({
-    role: initialFilter(roleParam) ?? "",
-    service: initialFilter(serviceParam) ?? "",
-    action: initialFilter(actionParam) ?? "",
-  });
-
-  const navigate = useNavigate();
-
-  const filterEvents = (key: FilterKey, selectedValue: string) => {
-    setFilters((oldFilters) => ({ ...oldFilters, [key]: selectedValue }));
-    pushQueryParam(search, key, selectedValue);
-  };
-
-  useEffect(() => {
-    const tempFilteredEvents = events?.filter((event) => {
-      return (
-        (filters.role === "" || filters.role === event.role) &&
-        (filters.service === "" || filters.service === event.service) &&
-        (filters.action === "" || filters.action === event.action)
-      );
-    });
-    setFilteredEvents(tempFilteredEvents ?? []);
-  }, [filters, events]);
-
-  const pushHistory = useCallback(() => {
-    navigate(
-      `/hendelser?fromDate=${fom}&fromTime=${fromTime}&toDate=${tom}&toTime=${toTime}&role=${filters.role}&service=${filters.service}&action=${filters.action}`
-    );
-  }, [fom, tom, filters, fromTime, toTime, navigate]);
-
-  const pushQueryParam = (search: string, key: string, value: string) => {
-    let searchParams = new URLSearchParams(search);
-    searchParams.set(key, value);
-    navigate(`/hendelser?${searchParams.toString()}`);
-  };
-
-  useEffect(() => {
-    if (fom !== "" && tom !== "" && fromTime !== "" && toTime !== "") {
-      pushHistory();
-    }
-  }, [fom, tom, fromTime, toTime, pushHistory]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     callRequest();
   }, [callRequest]);
 
-  let uniqueRoles = Array.from(new Set(events?.map(({ role }) => role)));
-  let uniqueServices = Array.from(
-    new Set(events?.map(({ service }) => service))
+  const { filteredItems: filteredEvents, handleFilterChange } = useFilter(
+    events ?? [],
+    ["role", "service", "action"]
   );
-  let uniqueActions = Array.from(new Set(events?.map(({ action }) => action)));
 
-  const { items, requestSort, sortConfig } = useTableSorting(filteredEvents);
-
-  let eventsLength = 0;
-  if (items.length) {
-    eventsLength = items.length;
-  }
+  const {
+    items: filteredAndSortedEvents,
+    requestSort,
+    sortConfig,
+  } = useTableSorting(filteredEvents);
 
   const getClassNamesFor = (name: keyof EventInfo) => {
     if (!sortConfig) {
@@ -112,144 +66,30 @@ const EventsTable = () => {
     return sortConfig.key === name ? sortConfig.direction : undefined;
   };
 
-  const handleTimePickerChange = (
-    value: TimePickerValue,
-    toOrFrom: "to" | "from"
-  ) => {
-    let updateValue;
-    typeof value === "string"
-      ? (updateValue = value)
-      : (updateValue = value.toLocaleTimeString());
-
-    toOrFrom === "to" ? setToTime(updateValue) : setFromTime(updateValue);
-  };
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * pageSize;
+    const lastPageIndex = firstPageIndex + pageSize;
+    return filteredAndSortedEvents.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, pageSize, filteredAndSortedEvents]);
 
   return (
     <>
-      <div className="row">
-        <div className="column">
-          <table id={"timetable"}>
-            <tbody>
-              <tr>
-                <th>Fra og med dato: </th>
-                <th>
-                  <Datepicker
-                    locale={"nb"}
-                    inputId="datepicker-event-input-fom"
-                    value={fom}
-                    onChange={setFom}
-                    inputProps={{
-                      name: "dateInput",
-                      "aria-invalid":
-                        fom !== "" && isISODateString(fom) === false,
-                    }}
-                    calendarSettings={{ showWeekNumbers: false }}
-                    showYearSelector={true}
-                  />
-                </th>
-                <th>
-                  <TimePicker
-                    onChange={(value) => handleTimePickerChange(value, "from")}
-                    value={fromTime}
-                  />
-                </th>
-              </tr>
-              <tr>
-                <th>Til og med:</th>
-                <th>
-                  <Datepicker
-                    locale={"nb"}
-                    inputId="datepicker-event-input-tom"
-                    value={tom}
-                    onChange={setTom}
-                    inputProps={{
-                      name: "dateInput",
-                      "aria-invalid":
-                        tom !== "" && isISODateString(tom) === false,
-                    }}
-                    calendarSettings={{ showWeekNumbers: false }}
-                    showYearSelector={true}
-                  />
-                </th>
-                <th>
-                  <TimePicker
-                    onChange={(value) => handleTimePickerChange(value, "to")}
-                    value={toTime}
-                  />
-                </th>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className="column">
-          <table>
-            <tbody>
-              <tr>
-                <th>
-                  <Select
-                    id={"select"}
-                    onChange={(event) =>
-                      filterEvents("role", event.target.value)
-                    }
-                    selected={filters.role}
-                  >
-                    <option value="">Velg rolle</option>
-                    {uniqueRoles.map((role) => {
-                      return (
-                        role && (
-                          <option key={role} value={role}>
-                            {role}
-                          </option>
-                        )
-                      );
-                    })}
-                  </Select>
-                </th>
-                <th>
-                  <Select
-                    id={"select"}
-                    onChange={(event) =>
-                      filterEvents("service", event.target.value)
-                    }
-                    selected={filters.service}
-                  >
-                    <option value="">Velg service</option>
-                    {uniqueServices.map((service) => {
-                      return (
-                        service && (
-                          <option key={service} value={service}>
-                            {service}
-                          </option>
-                        )
-                      );
-                    })}
-                  </Select>
-                </th>
-                <th>
-                  <Select
-                    id={"select"}
-                    onChange={(event) =>
-                      filterEvents("action", event.target.value)
-                    }
-                    selected={filters.action}
-                  >
-                    <option value="">Velg action</option>
-                    {uniqueActions.map((action) => {
-                      return (
-                        action && (
-                          <option key={action} value={action}>
-                            {action}
-                          </option>
-                        )
-                      );
-                    })}
-                  </Select>
-                </th>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <Filter
+        fromDate={debouncedFromDate}
+        fromTime={debouncedFromTime}
+        toDate={debouncedToDate}
+        toTime={debouncedToTime}
+        onFromDateChange={setFromDate}
+        onFromTimeChange={setFromTime}
+        onToDateChange={setToDate}
+        onToTimeChange={setToTime}
+        messages={events ?? []}
+        onFilterChange={handleFilterChange}
+        filterKeys={["service", "action", "role"]}
+      />
+      <span style={{ position: "relative", float: "left", margin: "20px 0" }}>
+        {filteredEvents.length} eventer
+      </span>
       <table className="tabell tabell--stripet">
         <thead>
           <tr>
@@ -329,7 +169,7 @@ const EventsTable = () => {
         </thead>
         <tbody>
           {!loading &&
-            items.map((event, index) => {
+            currentTableData.map((event, index) => {
               return (
                 <tr key={event.mottakid + index}>
                   <td className="tabell__td--sortert">{event.hendelsedato}</td>
@@ -353,10 +193,16 @@ const EventsTable = () => {
               );
             })}
         </tbody>
-        <caption>{eventsLength} hendelser</caption>
       </table>
       {loading && <NavFrontendSpinner />}
       {error?.message && <p>{error.message}</p>}
+      <Pagination
+        totalCount={filteredEvents.length}
+        pageSize={pageSize}
+        siblingCount={1}
+        currentPage={currentPage}
+        onPageChange={setCurrentPage}
+      />
     </>
   );
 };
