46f6d79fc837fcdcf484495e1afc061f46d0a905
Camilla <camillamdalan@gmail.com>
2022-02-28 13:02:21 +0100

    :bug: Fix app crashing on timestamp value = null

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -69,11 +69,13 @@ const Filter = <T, K extends keyof T>({
             showYearSelector={true}
           />
           <TimePicker
-            onChange={(value) =>
-              typeof value === "string"
-                ? onFromTimeChange(value)
-                : onFromTimeChange(value.toLocaleTimeString())
-            }
+            onChange={(value) => {
+              if (value !== null) {
+                typeof value === "string"
+                  ? onFromTimeChange(value)
+                  : onFromTimeChange(value.toLocaleTimeString());
+              }
+            }}
             value={fromTime}
           />
         </div>
@@ -153,11 +155,13 @@ const Filter = <T, K extends keyof T>({
             showYearSelector={true}
           />
           <TimePicker
-            onChange={(value) =>
-              typeof value === "string"
-                ? onToTimeChange(value)
-                : onToTimeChange(value.toLocaleTimeString())
-            }
+            onChange={(value) => {
+              if (value !== null) {
+                typeof value === "string"
+                  ? onToTimeChange(value)
+                  : onToTimeChange(value.toLocaleTimeString());
+              }
+            }}
             value={toTime}
           />
         </div>
