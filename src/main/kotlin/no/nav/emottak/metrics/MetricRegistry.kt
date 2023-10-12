cdb6be2ed3e516b31eedfa35d1653bee62918ce4
Nabil Fario <nabil.fario@nav.no>
2023-10-12 13:54:33 +0200

    La til grafana counter for hemtmeldinger og henthendelser

--- src/main/kotlin/no/nav/emottak/metrics/MetricRegistry.kt
+++ src/main/kotlin/no/nav/emottak/metrics/MetricRegistry.kt
@@ -2,13 +2,12 @@ package no.nav.emottak.metrics
 
 import io.prometheus.client.Counter
 
-const val METRICS_EM = "emottak-monitor"
 val hentmeldingerCounter: Counter =
-    Counter.build().namespace(METRICS_EM).name("hentmeldinger_count")
+    Counter.build().namespace("emottak-monitor").name("hentmeldinger_count")
         .help("Counts the number of api calls to hentmeldinger")
         .register()
 
 val henthendelserCounter: Counter =
-    Counter.build().namespace(METRICS_EM).name("henthendelser_count")
+    Counter.build().namespace("emottak-monitor").name("henthendelser_count")
         .help("Counts the number of api calls to henthendelser")
         .register()
