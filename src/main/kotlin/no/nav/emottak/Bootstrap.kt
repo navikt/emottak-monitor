422640a47a681505617e6fb61983a0999ec897bd
Nabil Fario <nabilfario@Nabils-MBP.lan>
2023-10-13 14:50:39 +0200

    Tester grafana

--- src/main/kotlin/no/nav/emottak/Bootstrap.kt
+++ src/main/kotlin/no/nav/emottak/Bootstrap.kt
@@ -17,7 +17,7 @@ import java.util.concurrent.TimeUnit
 
 val log: Logger = LoggerFactory.getLogger("no.nav.emottak.emottakMonitor")
 val hentmeldingerCounter: Counter =
-    Counter.build().namespace("emottak-monitor").name("hentmeldinger_count")
+    Counter.build().namespace("emottak_monitor").name("hentmeldinger_count")
         .help("Counts the number of api calls to hentmeldinger")
         .register()
 
