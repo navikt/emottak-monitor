33ec5a8ba5d725744381409ec180c5f562fa0710
Nabil Fario <nabil.fario@nav.no>
2023-10-17 09:11:12 +0200

    url endring

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,15 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+var sslRootCAs = require("ssl-root-cas");
+sslRootCAs.inject();
+
 module.exports = function (app) {
   app.use(
     "/v1",
     createProxyMiddleware({
-      target: "https://emottak-monitor.dev.intern.nav.no",
+      target: "https://emottak-monitor.intern.dev.nav.no",
       changeOrigin: true,
+      secure: false,
     })
   );
 };
