da406f66cf5c5ee372b42e0f5e04f1a5e9013d83
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-09-19 10:52:57 +0200

    Lagt til debug logging #2

--- backend/src/main/kotlin/no/nav/emottak/application/api/RegistrerMeldingerApi.kt
+++ backend/src/main/kotlin/no/nav/emottak/application/api/RegistrerMeldingerApi.kt
@@ -192,7 +192,7 @@ private suspend fun RoutingContext.executeREST(url: String) {
         }
     }
     catch (e: Exception) {
-        log.error("Feil ved kall mot $url", e)
+        log.error("Feil ved kall mot $url: ${e.message}", e)
         call.respond(HttpStatusCode.InternalServerError, "Feil ved kall mot $url: ${e.message}")
     }
 }
