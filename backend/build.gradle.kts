5510fca274d26e6259fa2acb3f701d80ce5f31f3
terjenilssen <terje.nilssen@nav.no>
2025-12-05 12:35:31 +0100

    Tatt vekk kluentVersion som ikke lenger brukes.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -6,7 +6,6 @@ version = "1.0.0"
 
 val coroutinesVersion = "1.10.2"
 val jacksonVersion = "2.18.2"
-val kluentVersion = "1.73"
 val ktorVersion = "3.1.3"
 val kotestVersion = "5.9.1"
 val logbackVersion = "1.5.19"
