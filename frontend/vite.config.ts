47c4e88f85da05f752e1dae35b13b0a3d8e1de62
terjenilssen <terje.nilssen@nav.no>
2026-06-17 15:01:46 +0200

    - Slettet ubenyttet style.css-fil. - Rullet tilbake endringen som var gjort i package.json under scripts => start. - Fjernet lessIgnorePlugin fra vite.config.ts som ser ut til å herpe nesten all CSS på nettsiden.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const lessIgnorePlugin = {
-    name: 'less-ignore',
-    setup(build: any) {
-        build.onLoad({ filter: /\.less$/ }, () => ({
-            contents: '',
-            loader: 'css',
-        }))
-    },
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    optimizeDeps: {
-        esbuildOptions: {
-            plugins: [lessIgnorePlugin],
-        },
-    },
-    server: {proxy:{"/v1":{target: `${process.env.PROXY_URL }`,
+    server: {
+        proxy:{
+            "/v1": {
+                target: `${ process.env.PROXY_URL }`,
                 changeOrigin: true,
-                secure: false,}}},
+                secure: false
+            }
+        }
+    },
     build: {outDir:"build"},
 })
